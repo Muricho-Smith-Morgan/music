c60030d2d4803b2cb0f057a95fb56037f779c3fa
brocode <kelimbth@gmail.com>
2024-04-13 14:41:21 +0300

    wrapped STATICFILES_DIRS & STATIC_ROOT in an if block

--- music_school/settings.py
+++ music_school/settings.py
@@ -125,16 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/css'),
-    os.path.join(BASE_DIR, 'static/images'),
-    os.path.join(BASE_DIR, 'static/js'),
-    os.path.join(BASE_DIR, 'static/bootstrap/icons'),
-    os.path.join(BASE_DIR, 'static/bootstrap/css'),
-    os.path.join(BASE_DIR, 'static/bootstrap/js'),
-
-]
-
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR, 'static',]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
