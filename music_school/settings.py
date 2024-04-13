b68e365410325f4a62c971a37527fe27c22e5ef8
brocode <kelimbth@gmail.com>
2024-04-13 14:39:38 +0300

    added environ module and added environment variable - SECRET_KEY

--- music_school/settings.py
+++ music_school/settings.py
@@ -11,17 +11,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-idn&gl5wua^z_&((9nlaap^xi%of!!j200jrg%^nujw#po*$&a'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
