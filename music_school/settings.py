92b2957a3518ea5b46b205c571419688109ffa17
brocode <kelimbth@gmail.com>
2024-04-13 14:43:45 +0300

    updated ALLOWED_HOSTS

--- music_school/settings.py
+++ music_school/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('WEBSITE_DOMAIN')]
 
 
 # Application definition
